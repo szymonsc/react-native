bd1f6dd9d3186977484681e67c1a6905116dfed0
Szymon Zakrzewski <szymon.zakrzewski@schange.com>
2022-08-03 00:58:19 +0200

    Updated android aar

--- ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactHorizontalScrollContainerView.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactHorizontalScrollContainerView.java
@@ -7,6 +7,7 @@
 
 package com.facebook.react.views.scroll;
 
+import android.util.Log;
 import android.content.Context;
 import androidx.core.view.ViewCompat;
 import com.facebook.react.modules.i18nmanager.I18nUtil;
@@ -44,9 +45,11 @@ public class ReactHorizontalScrollContainerView extends ReactViewGroup {
 
   @Override
   protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+    Log.e("Test", "Test onLayout");
     if (mLayoutDirection == LAYOUT_DIRECTION_RTL) {
       // When the layout direction is RTL, we expect Yoga to give us a layout
       // that extends off the screen to the left so we re-center it with left=0
+      int newLeftX = 0;
       int newLeft = 0;
       int width = right - left;
       int newRight = newLeft + width;
